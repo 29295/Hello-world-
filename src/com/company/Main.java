36cf3c799a4ea46c25b03ef17d5d3ed506193f19
yordanos <yordanos.negash.2021@student.lhschools.org>
2020-03-05 13:50:57 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+
+        System.out.println("helloWorld my name is yordanos");
+        System.out.println("i make buy a lot of clothes and shoes  ");
+        System.out.println("i visit a lot of places and new type of food ");
+
+    }
+}
+
+
